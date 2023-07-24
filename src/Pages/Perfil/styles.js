9a898d021b2e2edd659a9d7cbcbb6119d84522f5
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2023-07-23 21:32:18 -0300

    Responsividade

--- src/Pages/Perfil/styles.js
+++ src/Pages/Perfil/styles.js
@@ -1,66 +1,123 @@
-import styled from 'styled-components';
+import { styled } from "styled-components";
 import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   background: url('./Imagem/SilasBackground.png') no-repeat center;
   background-size: cover;
-  width: 100%;
-  min-height: 100vh;
+  width: 110%;
+  height: auto;
   background-color: #000000;
+  overflow: hidden;
   padding: 0 10px;
   padding-right: 65px;
 
+
   @media (max-width: 768px) {
+    width: 150%;
     padding-right: 16px;
     padding-left: 16px;
   }
 `;
 
 export const Content = styled.div`
+  height: 200px;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  max-width: 1200px;
-  margin: 0 auto;
+  width: 100%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    height: auto;
+  }
 `;
 
 export const Logo = styled.img`
+  display: inline-block;
   height: 172px;
   width: 280px;
-  margin: 12px 0;
+  margin-left: 123px;
+  margin-top: 12px;
 
   @media (max-width: 768px) {
-    margin: 20px 0;
+    margin-left: -33%;
+    margin-top: 20px;
   }
 `;
 
 export const NavContent = styled.div`
+  margin-top: 30px;
+  margin-left: 20%;
   display: flex;
-  align-items: center;
-  gap: 20px;
+  width: 431px;
+  height: 23px;
 
   @media (max-width: 768px) {
+    margin-left: -33%;
+  }
+
+  @media (max-width: 425px) {
+    margin-left: 30%;
+
     flex-direction: column;
+    display: flex;
+    width: 431px;
+    margin-top: 0px;
+    margin-bottom: 35%;
+
+  }
+
+  @media (max-width: 375px) {
+    margin-left: 35%;
+
+    flex-direction: column;
+    display: flex;
+    width: 431px;
+    margin-top: 0px;
+    margin-bottom: 200px;
+  }
+
+  @media (max-width: 320px) {
+    margin-left: 55%;
+    margin-bottom: 140px;
+
+
+    flex-direction: column;
+    display: flex;
+    width: 431px;
+    margin-top: 0px;
   }
 `;
 
+
+
 export const Navs = styled(Link)`
+
+  width: 68px;
+  height: 19px;
   font-family: 'Montserrat';
+  font-style: normal;
+  margin-top: 30px;
   font-weight: 400;
   font-size: 20px;
+  line-height: 24px;
   text-decoration: none;
   color: #ffff;
-  transition: transform 0.2s;
+
+
+  flex: none;
+  order: 1;
+  flex-grow: 1;
 
   &:hover {
     transform: scale(1.2); 
   }
 
-  @media (max-width: 768px) {
+  @media (max-width: 320px) {
     margin-top: 20px;
   }
 `;
+
+
 
 export const PerfilTitle = styled.div`
   text-align: center;
@@ -85,8 +142,36 @@ export const PerfilContent = styled.div`
   max-width: 800px;
 
   @media (max-width: 768px) {
-    margin-left: 5%;
-    margin-top: 12%;
+    margin-left: 8%;
+    width: 550px;
+    margin-top: 10%;
+  }
+
+  @media (max-width: 667px) {
+    margin-left: 2%;
+    width: 450px;
+    margin-top: 10%;
+  }
+
+  @media (max-width: 465px) {
+    margin-left: 1%;
+    width: 340px;
+    margin-top: 10%;
+  }
+
+  @media (max-width: 425px) {
+    margin-left: 1%;
+    width: 340px;
+  }
+
+  @media (max-width: 375px) {
+    margin-left: 10%;
+    width: 240px;
+  }
+
+  @media (max-width: 320px) {
+    margin-left: 10%;
+    width: 240px;
   }
 `;
 
@@ -111,18 +196,26 @@ export const PerfilText = styled.p`
   margin-bottom: 20px;
 
   @media (max-width: 768px) {
-    font-size: 2.7vw;
+    font-size: 5vw;
   }
 `;
 
 export const CompanyContent = styled.div`
   margin-top: 5%;
   text-align: center;
-  margin-left: -5%;
+  margin-left: -20%;
   margin-bottom: 10%;
+
+  @media (max-width: 768px) {
+    margin-left: -35%;
+  }
 
   @media (max-width: 425px) {
     margin-top: 10%;
+  }
+
+  @media (max-width: 320px) {
+    margin-left: -42%;
   }
 `;
 
@@ -135,8 +228,12 @@ export const History = styled.div`
     max-width: 600px;
   }
 
+  @media (max-width: 425px) {
+    max-width: 400px;
+  }
+
   @media (max-width: 384px) {
-    max-width: 370px;
+    max-width: 320px;
   }
 
   @media (max-width: 360px) {
@@ -144,7 +241,7 @@ export const History = styled.div`
   }
 
   @media (max-width: 320px) {
-    max-width: 314px;
+    max-width: 310px;
   }
 `;
 
@@ -158,21 +255,32 @@ export const Textp = styled.p`
 `;
 
 export const FooterPerfil = styled.div`
-  width: 100%;
   background-color: #242424;
   padding: 20px;
-  display: flex;
+  display: block;
+  width: 100%;
+  padding-left: 65px;
+  height: auto;
   justify-content: center;
 `;
 
 export const FooterContentWrapper = styled.div`
   width: 100%;
   max-width: 1200px;
+  margin-left: 5%;
+
+  @media (max-width: 768px) {
+    margin-left: -5%;
+  }
+
+  @media (max-width: 425px) {
+    margin-left: -10%;
+  }
 `;
 
 export const PerfisWrapper = styled.div`
   display: flex;
-  gap: 20px;
+  margin: 2% 0;
 
   @media (max-width: 768px) {
     flex-direction: column;
@@ -182,8 +290,9 @@ export const PerfisWrapper = styled.div`
 
 export const PerfisContents = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  gap: 10px;
+  margin: 2% 0;
 
   @media (max-width: 768px) {
     margin-top: 20px;
@@ -196,6 +305,7 @@ export const PerfilImage = styled.img`
 `;
 
 export const PerfilContentTitle = styled.h3`
+  margin-top: 10px;
   color: #FFF;
   font-size: 24px;
   font-family: Montserrat;
@@ -203,9 +313,18 @@ export const PerfilContentTitle = styled.h3`
 `;
 
 export const PerfilDescription = styled.p`
+  margin-top: 5px;
   color: #FFF;
   font-size: 17px;
   font-family: Montserrat;
+
+  @media (max-width: 768px) {
+    width: 300px;
+  }
+
+  @media (max-width: 320px) {
+    width: 200px;
+  }
 `;
 
 
@@ -248,30 +367,47 @@ export const CompanyFContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 20px;
+  margin: 0% 0;
 
   @media (max-width: 768px) {
-    margin-top: 0;
+    margin-top: 20px;
+    margin-right: 10%;
   }
 `;
 
 export const CompanyTitle = styled.h3`
   color: #fff;
-  font-size: 5vw;
+  font-size: 24px;
   font-family: Montserrat;
   font-weight: 800;
+
+  @media (max-width: 320px) {
+    width: 300px;
+    margin-right: -80px;
+  }
 `;
 
 export const CompanyDescription = styled.p`
-  font-size: 3vw;
+  font-size: 24px;
   font-weight: 400;
   color: #ffffff;
   font-family: Montserrat;
   margin-top: 8px;
   text-align: center;
 
+  @media (max-width: 768px) {
+    font-size: 24px;
+    width: 600px;
+  }
+
   @media (max-width: 425px) {
-    font-size: 4vw;
+    width: 300px;
+    margin-right: 5px;
+  }
+
+  @media (max-width: 320px) {
+    width: 300px;
+    margin-right: 50px;
   }
 `;
 
@@ -280,27 +416,28 @@ export const ContactContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 20px 0;
+  margin: 2% 0;
 
   @media (max-width: 768px) {
-    margin-left: 0;
+    margin-top: 20px;
+  }
+
+  @media (max-width: 425px) {
+    margin-top: 20px;
+    margin-right: 60px;
   }
 `;
 
 export const ContactTitle = styled.h3`
   color: #fff;
-  font-size: 5vw;
   font-family: Montserrat;
+  font-size: 24px;
   font-weight: 800;
-
-  @media (max-width: 768px) {
-    font-size: 3.5vw;
-    margin-left: -10px;
-  }
 `;
 
 export const ContactList = styled.ul`
   list-style: none;
+  font-size: 2vw;
   padding: 0;
   margin: 0;
   color: #fff;
@@ -308,16 +445,14 @@ export const ContactList = styled.ul`
   font-family: Montserrat;
 
   @media (max-width: 768px) {
-    font-size: 2.5vw;
   }
 `;
 
 export const ContactListItem = styled.li`
-  font-size: 2.5vw;
+  font-size: 16px;
   color: #ffffff;
   margin-bottom: 8px;
 
   @media (max-width: 768px) {
-    font-size: 2vw;
   }
 `;
