5915f266a83aa9263209a248db326cab1c390987
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2023-10-02 19:48:11 -0300

    Atualização

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -6,23 +6,6 @@ export const Footer = () => {
     <>
       <C.FooterPerfil>
         <C.FooterContentWrapper>
-      <C.PerfisWrapper>
-        <C.PerfisContents>
-            <C.PerfilImage src="./Imagem/Icons/Group.svg" alt="Imagem do perfil" />
-            <C.PerfilContentTitle>Atendimento</C.PerfilContentTitle>
-            <C.PerfilDescription>Relacionamento com o cliente é o nosso principal pilar.</C.PerfilDescription>
-        </C.PerfisContents>
-          <C.PerfisContents>
-            <C.PerfilImage src="./Imagem/Icons/Estrela.svg" alt="Imagem do perfil" />
-            <C.PerfilContentTitle>Qualidade</C.PerfilContentTitle>
-            <C.PerfilDescription>Silaços é reconhecida por sua agilidade na entrega de produtos sob medida.</C.PerfilDescription>
-        </C.PerfisContents>
-          <C.PerfisContents>
-            <C.PerfilImage src="./Imagem/Icons/Pagamento.svg" alt="Imagem do perfil" />
-            <C.PerfilContentTitle>Pagamento</C.PerfilContentTitle>
-            <C.PerfilDescription>Aceitamos PIX, Cartão de crédito e Faturamento.</C.PerfilDescription>
-        </C.PerfisContents>
-        </C.PerfisWrapper>
       <C.ContentF>
         <C.LogoContent>
             <C.LogoF src="./Imagem/LogoSerralheria.png" alt="Logo da Empresa" />
@@ -38,7 +21,7 @@ export const Footer = () => {
         <C.ContactList>
             <C.ContactListItem>Whatsapp: +55 16 99154-7295 </C.ContactListItem>
             <C.ContactListItem>Email: s.panissa@uol.com.br</C.ContactListItem>
-            <C.ContactListItem>Telefone: ( 16 )   3442-8384</C.ContactListItem>
+            <C.ContactListItem>Telefone: (16) 99154-7295</C.ContactListItem>
         </C.ContactList>
     </C.ContactContent>
       </C.ContentF>
